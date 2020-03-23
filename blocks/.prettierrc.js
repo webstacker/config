428ec2e40b55e75443356c890cb127c8ac9d787e
Stephen S <webstacker@outlook.com>
2020-03-23 14:08:55 +0000

    Initial commit

---
+++ blocks/.prettierrc.js
@@ -0,0 +1,12 @@
+/*! m0-start */
+const config = {
+    tabWidth: 4,
+    singleQuote: true,
+    printWidth: 100,
+    bracketSpacing: false
+};
+/*! m0-end */
+
+/*! m0-start */
+module.exports = config;
+/*! m0-end */
---
+++ index.js
@@ -0,0 +1,35 @@
+const createModuleZero = require('module-zero');
+
+const m0 = createModuleZero({
+    files: '**/*',
+    devDependencies: {
+        "eslint": "^6.8.0",
+        "eslint-config-airbnb-base": "^14.0.0",
+        "eslint-config-prettier": "^6.9.0",
+        "eslint-plugin-import": "^2.19.1",
+        "eslint-plugin-jest": "^23.3.0",
+        "eslint-plugin-prettier": "^3.1.2",
+        "fs-extra": "^8.1.0",
+        "husky": "^4.0.3",
+        "jest": "^24.9.0",
+        "lint-staged": "^9.5.0",
+        "prettier": "^1.19.1"
+    },
+    blocks: {
+        src: '**/*',
+        commentStyles: {
+            '#': '#! m0',
+            '//': '//! m0',
+            '/**/': '/*! m0 */'
+        },
+        commentStyleMap: {
+            '.gitignore': '#',
+            '.eslintignore': '#',
+            '.js': '/**/'
+        }
+    }
+});
+
+m0.copyFiles();
+m0.createBlocks();
+m0.installDevDependencies();
